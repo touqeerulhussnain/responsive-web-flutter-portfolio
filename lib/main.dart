340cd49a1044cf9e4e0a9849a82e1c739c27ce98
Touqeer <touqeerulhussnain@gmail.com>
2025-12-13 15:22:24 +0500

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'core/constants/firebase_options.dart';
 import 'core/utils/dprint.dart';
 import 'view/home_view/home_view.dart';
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBO2YlENtUBYA7Tl8U7H3oyUpYKQFBiLUQ",
-//   authDomain: "responsive-flutter-portfolio.firebaseapp.com",
-//   projectId: "responsive-flutter-portfolio",
-//   storageBucket: "responsive-flutter-portfolio.firebasestorage.app",
-//   messagingSenderId: "780548286752",
-//   appId: "1:780548286752:web:607f2bc7dcd79e05a80f45"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 void main() async {
   try {
     await Firebase.initializeApp(options: options());
